7410d1e2a7e537bdb3d85512403e9f0956f44082
xxoppyxx <oppy.luca@gmail.com>
2016-03-31 16:29:50 +0200

    torre indentato giusto

--- Esercitazioni Java/src/extra/scacchi/Pezzo.java
+++ Esercitazioni Java/src/extra/scacchi/Pezzo.java
@@ -14,24 +14,26 @@ abstract class Pezzo {
 	public abstract boolean mossaValida(Scacchiera s, Casella c1, Casella c2);
 	public abstract String toString();
 	
-	class Torre extends Pezzo {
 
-		@Override
-		public boolean mossaValida(Scacchiera s, Casella c1, Casella c2) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	}
 }
 
 class Pedone extends Pezzo{
+
+	@Override
+	public boolean mossaValida(Scacchiera s, Casella c1, Casella c2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
+
+class Torre extends Pezzo {
 
 	@Override
 	public boolean mossaValida(Scacchiera s, Casella c1, Casella c2) {
