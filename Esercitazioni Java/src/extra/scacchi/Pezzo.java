4a86e244489da35c4863f5a1f5513ef301d3e11c
xxoppyxx <oppy.luca@gmail.com>
2016-03-31 16:24:37 +0200

    torre bozza

--- Esercitazioni Java/src/extra/scacchi/Pezzo.java
+++ Esercitazioni Java/src/extra/scacchi/Pezzo.java
@@ -13,6 +13,22 @@ abstract class Pezzo {
 	
 	public abstract boolean mossaValida(Scacchiera s, Casella c1, Casella c2);
 	public abstract String toString();
+	
+	class Torre extends Pezzo {
+
+		@Override
+		public boolean mossaValida(Scacchiera s, Casella c1, Casella c2) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
 }
 
 
